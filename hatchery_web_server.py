5d15f3a33ac4fcc4bdcf8300f09cd06681d4fa0f
jdpelletier <john.pelletier@ymail.com>
2020-10-21 17:07:26 -1000

    adding login page

--- hatchery_web_server.py
+++ hatchery_web_server.py
@@ -1,12 +1,51 @@
 import os
 import datetime
 
-from flask import Flask, redirect, request, url_for
-from home import home
+from werkzeug.urls import url_parse
+from flask import Flask, request, render_template, flash, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, current_user, login_user, logout_user, login_required, UserMixin
+from flask_migrate import Migrate
+from werkzeug.security import generate_password_hash, check_password_hash
+
+from config import Config
 
 app = Flask(__name__)
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+login = LoginManager(app)
+login.login_view = 'login'
+
+from home import home
+from forms import LoginForm
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User}
 
 @app.route('/',methods=['POST', 'GET'])
+@login_required
 def home_():
     parent_directory = "/home/pi/Desktop/Hatchery/TestData"
     ## for testing button, erase later
@@ -19,7 +58,28 @@ def home_():
     filepath = os.path.join(str(path), "dataFile.txt")
     return home(filepath)
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home_'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('home_'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('home_')
+        return redirect(next_page)
+    return render_template('login.html', title='Sign In', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
 if __name__ == '__main__':
-    host = '0.0.0.0'
-    port = 50009
-    app.run(host=host,port=port)
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=50009)
