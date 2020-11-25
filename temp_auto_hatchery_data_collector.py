10d2c989f5c486494fa0bfa4d38bbdaa4e1ee601
jdpelletier <john.pelletier@ymail.com>
2020-11-25 12:48:01 -1000

    update with nick params

--- temp_auto_hatchery_data_collector.py
+++ temp_auto_hatchery_data_collector.py
@@ -13,9 +13,9 @@ def hatchery_data_collector():
             path = Util.FolderCreate(currentDay)
         #Taking average of 10 measurements
         t1, t2, t3, p = Util.serRead()
-        if t2 or t3 >= 86.0: #TODO get temp value from nick
+        if t2 or t3 >= 85.0: #TODO get temp value from nick
             Util.pump_on()
-            time.sleep(5)    #TODO get time from nick
+            time.sleep(60)    #TODO get time from nick
             Util.pump_off()
             t1, t2, t3, p = Util.serRead()
         string = f"{t1} {t2} {t3} {p}"
