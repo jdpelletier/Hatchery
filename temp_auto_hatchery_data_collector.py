2de512b2355760f4336f3c7d09cf509349df66fc
jdpelletier <john.pelletier@ymail.com>
2020-12-01 11:39:35 -1000

    adding if statement for max temp

--- temp_auto_hatchery_data_collector.py
+++ temp_auto_hatchery_data_collector.py
@@ -17,11 +17,12 @@ def hatchery_data_collector():
         if pumprunning == True: #if pump was on before read, cycle it back on after
             pumprunning = Util.pump_off()
             pumprunning = Util.pump_on()
-        while t2 or t3 > 80.0:
-            Util.pump_on()
-            time.sleep(120)
-            Util.pump_off()
-            t1, t2, t3, p = Util.serRead()
+        if t2 or t3 > 78:
+            while t2 or t3 > 75:
+                Util.pump_on()
+                time.sleep(120)
+                Util.pump_off()
+                t1, t2, t3, p = Util.serRead()
         string = f"{t1} {t2} {t3} {p}"
         Util.FileWrite(path, string)
         time.sleep(288)            # wait 5 minutes
