4b4659d61f089d6e001eb8885c4c1657312c1ea8
jdpelletier <john.pelletier@ymail.com>
2021-03-11 23:54:03 -1000

    starting 2 setup webserver

---
+++ hatchery_web_server2setups.py
@@ -0,0 +1,104 @@
+import os
+import datetime
+
+from werkzeug.urls import url_parse
+from flask import Flask, request, render_template, flash, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, current_user, login_user, logout_user, login_required, UserMixin
+from flask_migrate import Migrate
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_bootstrap import Bootstrap
+
+from config import Config
+
+app = Flask(__name__)
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+login = LoginManager(app)
+login.login_view = 'login'
+bootstrap = Bootstrap(app)
+
+from home2setups import home
+from history import history
+from forms import LoginForm
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User}
+
+@app.route('/',methods=['POST', 'GET'])
+@login_required
+def home_():
+    parent_directory = "/home/pi/Desktop/Hatchery/TestData"
+    today = datetime.date.today()
+    todaystr = today.isoformat()
+    path = os.path.join(parent_directory, todaystr)
+    filepath = os.path.join(str(path), "dataFile.txt")
+    return home(filepath)
+
+@app.route('/history',methods=['POST', 'GET'])
+@login_required
+def history_():
+    parent_directory = "/home/pi/Desktop/Hatchery/TestData/"
+    today = datetime.date.today()
+    day = request.form.get("day", str(today.day))
+    if len(day) == 1:
+        day = "0" + day
+    month = request.form.get("month", str(today.month))
+    if len(month) == 1:
+        month = "0" + month
+    year = request.form.get("year", str(today.year))
+    daystring = f"{year}-{month}-{day}"
+    path = parent_directory + daystring
+    filepath = os.path.join(str(path), "dataFile.txt")
+    submit = request.form.get("submit")
+    if submit:
+        return history(filepath, daystring, submit)
+    return history(filepath, daystring, submit)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home_'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('home_'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('home_')
+        return redirect(next_page)
+    return render_template('login.html', title='Sign In', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
+if __name__ == '__main__':
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=50009)
---
+++ home2setups.py
@@ -0,0 +1,152 @@
+from datetime import datetime, date
+import statistics
+
+from flask import Flask, render_template, request
+
+from bokeh.plotting import figure, output_file, show
+from bokeh.layouts import gridplot
+from bokeh.embed import components
+
+from Util import pump_on, pump_off, pump_check, auto_file_write, auto_check
+
+def home(path):
+    # prepare some data
+    time_arr = []
+    temp1 = []
+    temp2 = []
+    temp3 = []
+    ph = []
+    with open(path, 'r') as f:
+        for line in f:
+            data = line.split()
+            time = data[0].strip().strip('*\x00')
+            time = datetime.strptime(time, '%H:%M:%S')
+            today = date.today()
+            time = time.replace(year=today.year, month=today.month, day=today.day)
+            time_arr.append(time)
+            temp1.append(float(data[1]))
+            temp2.append(float(data[2]))
+            temp3.append(float(data[3]))
+            ph.append(float(data[4]))
+    t1_max = max(temp1)
+    t2_max = max(temp2)
+    t3_max = max(temp3)
+    t1_min = min(temp1)
+    t2_min = min(temp2)
+    t3_min = min(temp3)
+    ph_av = statistics.mean(ph)
+    datadic = {
+        "day" : today,
+        "time" : time.strftime("%H:%M:%S"),
+        "temp1" : float(data[1]),
+        "temp2" : float(data[2]),
+        "temp3" : float(data[3]),
+        "ph" : float(data[4])
+        }
+
+    t1_plot = figure(title="Temp 1", x_axis_label='Time', y_axis_label='Temp F',
+               x_axis_type='datetime')
+    t1_plot.sizing_mode = 'scale_width'
+    t1_plot.circle(time_arr, temp1, size=5)
+    t1_plot.line(time_arr, t1_max, legend_label="Max temp today: %f" % t1_max,
+                 line_color="red")
+    t1_plot.line(time_arr, t1_min, legend_label="Min temp today: %f" % t1_min,
+                 line_color="blue")
+    t1plots = {'plot' : t1_plot}
+    t1plot = components(t1plots)
+
+    t2_plot = figure(title="Temp 2", x_axis_label='Time', y_axis_label='Temp F',
+               x_axis_type='datetime')
+    t2_plot.sizing_mode = 'scale_width'
+    t2_plot.circle(time_arr, temp2, size=5)
+    t2_plot.line(time_arr, t2_max, legend_label="Max temp today: %f" % t2_max,
+                 line_color="red")
+    t2_plot.line(time_arr, t2_min, legend_label="Min temp today: %f" % t2_min,
+                 line_color="blue")
+    t2plots = {'plot' : t2_plot}
+    t2plot = components(t2plots)
+
+    t3_plot = figure(title="Temp 3", x_axis_label='Time', y_axis_label='Temp F',
+               x_axis_type='datetime')
+    t3_plot.sizing_mode = 'scale_width'
+    t3_plot.circle(time_arr, temp3, size=5)
+    t3_plot.line(time_arr, t3_max, legend_label="Max temp today: %f" % t3_max,
+                 line_color="red")
+    t3_plot.line(time_arr, t3_min, legend_label="Min temp today: %f" % t3_min,
+                 line_color="blue")
+    t3plots = {'plot' : t3_plot}
+    t3plot = components(t3plots)
+
+    ph_plot = figure(title="Fish Tank pH", x_axis_label='Time', y_axis_label='pH',
+               x_axis_type='datetime')
+    ph_plot.sizing_mode = 'scale_width'
+    ph_plot.circle(time_arr, ph, size=5)
+    ph_plot.line(time_arr, ph_av, legend_label="Average pH today: %f" % ph_av,
+                 line_color="green")
+    phplots = {'plot' : ph_plot}
+    phplot = components(phplots)
+
+    # p = gridplot([[t1_plot, t2_plot], [t3_plot, ph_plot]])
+    # p.sizing_mode = 'scale_width'
+    #
+    # script, div = components(p)
+
+    ##Button stuff##
+    pump1running = pump_check(1)
+    pump2running = pump_check(2)
+    pump1on = request.form.get("pump1on")
+    if pump1on:
+        pump1running = pump_check(1)
+        pump2running = pump_check(2)
+        if pump1running == False and pump2running == True:
+            pump1running = pump_on(3)
+        elif pump1running == False and pump2running == False:
+            pump1running = pump_on(1)
+
+    pump2on = request.form.get("pump2on")
+    if pump2on:
+        pump1running = pump_check(1)
+        pump2running = pump_check(2)
+        if pump1running == True and pump2running == False:
+            pump2running = pump_on(3)
+        elif pump1running == False and pump2running == False:
+            pump2running = pump_on(2)
+
+    pump1off = request.form.get("pump1off")
+    if pump1off:
+        pump1running = pump_check(1)
+        pump2running = pump_check(2)
+        if pump1running == True and pump2running == False:
+            pump1running = pump_off(1)
+        elif pump1running == True and pump2running == True:
+            pump1running = pump_off(1)
+            pump2running = pump_on(2)
+
+    pump2off = request.form.get("pump2off")
+    if pump2off:
+        pump1running = pump_check(1)
+        pump2running = pump_check(2)
+        if pump1running == False and pump2running == True:
+            pump2running = pump_off(2)
+        elif pump1running == True and pump2running == True:
+            pump2running = pump_off(2)
+            pump1running = pump_on(1)
+
+    autorunning = auto_check()
+    autoon = request.form.get("autoon")
+    if autoon:
+        if autorunning == False:
+            autorunning = auto_file_write('on')
+
+    autooff = request.form.get("autooff")
+    if autooff:
+        if autorunning == True:
+            autorunning = auto_file_write('off')
+
+
+    return render_template("home2setups.html", datadic=datadic, t1plot=t1plot,
+                            t2plot=t2plot, t3plot=t3plot, phplot=phplot,
+                            pump1on=pump1on, pump1off=pump1off, pump2on=pump2on,
+                            pump2off=pump2off, pump1running=pump1running,
+                            pump2running=pump2running, autoon=autoon,
+                            autooff=autooff, autorunning=autorunning)
